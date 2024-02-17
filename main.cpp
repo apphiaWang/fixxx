4ca5568a7b2abe886ebe9a9a5d936dedbff62873
ywa416 <ywa416@sfu.ca>
2024-02-16 20:45:46 -0800

    Merge: manually merge user authentication from Hugh to master

--- main.cpp
+++ main.cpp
@@ -2,13 +2,17 @@
 #include <filesystem>
 #include <fstream>
 #include <string>
+#include <cstring>
 #include <map>
 #include <vector>
 #include <sstream>
+#include <sys/stat.h>
+
 #include "commands.h"
 #include "fileUtils.h"
 #include "stringUtils.h"
 #include "userUtils.h"
+#include "authentication.h"
 
 #define USERNAME_MAX_LEN 20
 
@@ -55,6 +59,8 @@ void prompt()
 
 int main(int argc, char* argv[])
 {
+    const char *seed = "some random fake encryption template....";
+    
     if(argc != 2)
     {
         std::cout << "Invalid argument. Check user manual." << std::endl;
@@ -62,15 +68,44 @@ int main(int argc, char* argv[])
     }
     
     // Init filesystem root folder if not exist
-    if (fileSystemExist()) {
-        login(argv[1]);
+    struct stat st;
+    const char *folder_name = "filesystem";
+    const char *public_folder_name = "public_keys";
+    const char *private_folder_name = "filesystem/private_keys";
+    	
+    if (stat(folder_name, &st) == -1) {
+        if (errno == ENOENT) {
+            std::cout << "Initializing CMPT785 encrypted file system..." << std::endl;
+            mkdir(folder_name, S_IRWXU);
+            mkdir(public_folder_name, S_IRWXU);
+            mkdir(private_folder_name, S_IRWXU);
+            std::cout << "Generating key pair for " << argv[1] << "..." << std::endl;
+            std::string name_prefix(argv[1]); 
+            generate_key_pair(name_prefix);
+            std::cout << "Key pair generated." << std::endl;
+        } else {
+            // @TODO Some other error occurred.
+            return 0;
+        }
     } else {
-        initSystemRoot();
-        addUser(argv[1], true);
-        isAdmin = true;
+        if (S_ISDIR(st.st_mode)) {
+            std::cout << "Verifying...." << std::endl;
+            std::string name_prefix(argv[1]); 
+            bool is_valid = validate_login(name_prefix, seed);
+            if (is_valid) {
+                std::cout << "Login succeeded." << std::endl;                
+                // @TODO check if user is admin and track status, now assume user is always admin
+                isAdmin = true;
+            } else {
+                std::cout << "Login failed." << std::endl;
+                return 0;
+            }
+        } else {
+            return 0;
+        }
     }
-    
-    std::string info = "Encrypted Filsystem:\n\nAvailable Commands:\ncd <dir>\nls\npwd\nmkfile <file> <contents> \
+
+    std::string info = "CMPT785 Encrypted Filsystem:\n\nAvailable Commands:\ncd <dir>\nls\npwd\nmkfile <file> <contents> \
     \nmkdir <dir>\ncat <file>\nshare <file> <user>\nexit\n\nAdmin can also use:\nadduser <user>\n\nFilename/username constraints: \
     \nMax 20 characters. Can only contain 'A-Z','a-z','0-9','-','_','.','='.\nFile contents max length: 470 bytes.\n\n";
     std::cout << info << std::endl;
