c170453cf6eb3a8149ccfb45b476c907272f145f
ywa416 <ywa416@sfu.ca>
2024-02-20 01:31:49 -0800

    feat: add mkfile, cat commands

--- commands.h
+++ commands.h
@@ -8,7 +8,8 @@
 
 
 /* session info*/
-std::string currentUser;
+// @TODO team set currentUser and adminName when after program starts
+std::string currentUser = "admin";
 std::string adminName = "admin";
 bool isAdmin = false;
 
@@ -148,14 +149,11 @@ void mkdir(const std::string& dirname)
     // user should only mkdir under filesystem/<username>/personal
     auto relpath = split(locPath,'/');
     auto userOfLoc = encrypt_decrypt(relpath[0]);
-    if(userOfLoc.empty()) {
-        std::cout << "An error occurs, please contact admin" << std::endl;
-        return;
-    }
-    if(relpath.size() < 2) {
+    if(userOfLoc.empty() || userOfLoc != currentUser || relpath.size() < 2) {
         std::cout << "Can't create dir here" << std::endl;
         return;
     }
+
     auto expectHomeDir = encrypt_decrypt(relpath[1]);
     if(expectHomeDir.empty() or expectHomeDir != "personal") {
         std::cout << "Can't create dir here" << std::endl;
@@ -211,17 +209,121 @@ void ls()
             }
             std::cout << decryptedFile << std::endl;
         }
-        else if (isAdmin)
-        {   // only shows unencrypted dir/files to admin user
-            if (item.is_directory()) {
-                std::cout << "(unencrypted) d -> " << file << std::endl;
-            } else {
-               std::cout << "(unencrypted) f ->" << file << std::endl;
+        // @TODO check encryption decryption with team and design the behavior
+        // else if (isAdmin)
+        // {   // only shows unencrypted dir/files to admin user
+        //     if (item.is_directory()) {
+        //         std::cout << "(unencrypted) d -> " << file << std::endl;
+        //     } else {
+        //        std::cout << "(unencrypted) f ->" << file << std::endl;
                 
-            }
-        }
+        //     }
+        // }
     }
 }
 
+std::string encryptFileContent(std::string plaintext) {
+    return plaintext;
+}
+
+std::string decryptFileContent(std::string cipher) {
+    return cipher;
+}
+
+/*
+cat - print the content of the file
+*/
+void cat(const std::string& filename) {
+    std::string pathLoc = removePrefix(currentPath, FILE_SYSTEM_ROOT_PATH_STR);
+    std::string finalPath = "filesystem/" + pathLoc + "/" + encrypt_decrypt(filename);
+
+    std::ifstream file(finalPath);
+    if (file.is_open())
+    {
+        try {
+            std::stringstream buffer;
+            file >> buffer.rdbuf();
+            // TODO @Hugh implement file content ecryption
+            std::cout << decryptFileContent(buffer.str()) << std::endl;
+        }
+        catch (const std::exception& e) {
+            std::cout << "cat failed. Exception in decrypt: " << e.what() << std::endl;
+        }
+        file.close();
+    }
+    else
+    {
+        std::cout << filename << " doesn't exist" << std::endl;
+    }
+}
+
+
+/*
+mkfile - create a new text file
+*/
+void mkfile(const std::string& filename, std::string contents) {
+    // input validation
+    if (filename.empty() || filename.length() > FILENAME_MAX_LEN) 
+    {
+        std::cout << "Invaid file name, please check user manual" << std::endl;
+        return;
+    }
+    for (const char c : filename) 
+    {
+        if (!isalnum(c) && c != '_' && c != '-' && c != '.' && c != '=') {
+            std::cout << "Invaid file name, please check user manual" << std::endl;
+            return;
+        }
+    }
+    
+    // get the current location path and the target file path to create
+    std::string locPath;
+    auto canonicalFile = std::filesystem::weakly_canonical(currentPath / filename);
+    std::string filePath;
+    try {
+        locPath = removePrefix(currentPath, FILE_SYSTEM_ROOT_PATH_STR);
+        filePath = removePrefix(canonicalFile.u8string(), FILE_SYSTEM_ROOT_PATH_STR);
+    } catch (int error) {
+        std::cout << "Can't create file here" << std::endl;
+        return;
+    }
+
+    auto relPath = split(locPath, '/');
+    auto user = encrypt_decrypt(relPath[0]);
+    auto folderName = encrypt_decrypt(relPath[1]);
+    std::cout << user << currentUser << std::endl;
+    // Should only create files under filesystem/<user>/personal
+    if (user.empty() || user !=  currentUser || folderName.empty() || folderName == "shared") {
+        std::cout << "Can't create file here" << std::endl;
+        return;
+    }
+
+    // Encrypt filename and contents
+    std::string filenameEnc;
+    try {
+        filenameEnc = encrypt_decrypt(filename);
+        contents = encryptFileContent(contents);
+    } catch (const std::exception& e) {
+        std::cout << "mkfile failed. Exception in encrypt: " << e.what() << std::endl;
+        return;
+    }
+
+    // Check if filename already exists
+    if (std::filesystem::exists(currentPath / filenameEnc)) {
+        std::cout << "mkfile failed. File "<< filename << " already exists" << std::endl;
+        return;
+    }
+
+    // Create and write to file
+    std::ofstream file(currentPath / filenameEnc, std::ofstream::trunc);
+    if (!file.is_open()) {
+        std::cout << "Failed to create file" << std::endl;
+        return;
+    }
+    
+    file << contents;
+    file.close();
+    std::cout << "Successfully created file" << std::endl;
+}
 
 #endif // COMMANDS_H
